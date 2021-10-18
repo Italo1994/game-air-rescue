ff2f547f2fca80c70f04b762ab932e8137cf9e2e
Italo1994 <italohortiz2015@gmail.com>
2021-10-18 14:32:09 -0300

    Adicionando a movimentação de todos os personagens(amigo, inimigo1 e inimigo2)

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ function start() {
 
   // Principais variáveis do jogo
   var jogo = {};
+  var velocidade = 5;
+  var posicaoY = parseInt(Math.random() * 334);
   var TECLA = {
     W: 87,
     S: 83,
@@ -33,6 +35,9 @@ function start() {
   function loop() {
     moveFundo();
     moveJogador();
+    moveInimigo1();
+    moveInimigo2();
+    moveAmigo();
   } // Fim da função loop
 
   function moveFundo() {
@@ -43,13 +48,56 @@ function start() {
   function moveJogador() {
     if(jogo.pressionou[TECLA.W]) {
       var topo = parseInt( $("#jogador").css("top") );
-      $("#jogador").css("top", topo-10);
+
+      if(topo <= 0) {
+        $("#jogador").css("top", topo+10);
+      } else {
+        $("#jogador").css("top", topo-10);
+      }
     }
 
     if(jogo.pressionou[TECLA.S]) {
       var topo = parseInt( $("#jogador").css("top") );
-      $("#jogador").css("top", topo+10);
+
+      if(topo >= 434) {
+        $("#jogador").css("top", topo-10);
+      } else {
+        $("#jogador").css("top", topo+10);
+      }
     }
   } // Fim da função moveJogador
+
+  function moveInimigo1() {
+    let posicaoX = parseInt( $("#inimigo1").css("left") );
+    $("#inimigo1").css("left", posicaoX-velocidade);
+    $("#inimigo1").css("top", posicaoY);
+
+    if(posicaoX <= 0) {
+      var posicaoY = parseInt( Math.random() * 334 );
+      $("#inimigo1").css("left", 694);
+      $("#inimigo1").css("top", posicaoY);
+
+    }
+  } // Fim da função moveInimigo1
+
+  function moveInimigo2() {
+    let posicaoX = parseInt( $("#inimigo2").css("left") );
+    $("#inimigo2").css("left", posicaoX - 3);
+    $("#inimigo2").css("top", posicaoY);
+
+    if(posicaoX <= 0) {
+      var posicaoY = parseInt( Math.random() * 334 );
+      $("#inimigo2").css("left", 794);
+    }
+  } // Fim da função moveInimigo2
+
+  function moveAmigo() {
+    let posicaoX = parseInt( $("#amigo").css("left") );
+    $("#amigo").css("left", posicaoX + 1);
+
+    if(posicaoX >= 894) {
+      $("#amigo").css("left", 10);
+    }
+  } // Fim da função moveAmigo
 
 }
