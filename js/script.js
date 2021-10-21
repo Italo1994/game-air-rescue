070a1b9410651c1166aea713f4b5911c8838c9d6
Italo1994 <italohortiz2015@gmail.com>
2021-10-20 21:40:46 -0300

    Criando a função para detectar a colisão entre os dois helicópteros

--- js/script.js
+++ js/script.js
@@ -39,6 +39,7 @@ function start() {
     moveInimigo1();
     moveInimigo2();
     moveAmigo();
+    colisao();
   } // Fim da função loop
 
   function moveFundo() {
@@ -55,7 +56,7 @@ function start() {
       } else {
         $("#jogador").css("top", topo-10);
       }
-    }
+    } // move para cima
 
     if(jogo.pressionou[TECLA.S]) {
       var topo = parseInt( $("#jogador").css("top") );
@@ -65,7 +66,7 @@ function start() {
       } else {
         $("#jogador").css("top", topo+10);
       }
-    }
+    } // move para baixo
 
     if(jogo.pressionou[TECLA.D]) {
       // chama a função disparo
@@ -135,4 +136,40 @@ function start() {
 
   } // Fim da função disparo
 
-}
+  function colisao() {
+    var colisao1 = ($("#jogador").collision($("#inimigo1")));
+
+    if(colisao1.length > 0) {
+      let inimigo1X = parseInt( $("#inimigo1").css("left") );
+      let inimigo1Y = parseInt( $("#inimigo1").css("top") );
+
+      explosao1(inimigo1X, inimigo1Y);
+    }
+
+  } // Fim da função colisao
+
+  function explosao1(inimigo1X, inimigo1Y) {
+    $("#fundoGame").append("<div id='explosao1'></div>");
+    $("#explosao1").css("background-image", "url(imgs/explosao.png)");
+
+    var div = $("#explosao1");
+    div.css("top", inimigo1Y);
+    div.css("left", inimigo1X);
+    div.animate( {width: 200, opacity: 0}, "slow");
+
+    var tempoExplosao = window.setInterval(removeExplosao, 1000);
+
+    let posicaoY = parseInt( Math.random() * 334 );
+    $("#inimigo1").css("left", 704);
+    $("#inimigo1").css("top", posicaoY);
+
+    function removeExplosao() {
+      div.remove();
+      window.clearInterval(tempoExplosao);
+      tempoExplosao = null;
+
+    } // Fim da função removeExplosao
+
+  } // Fim da função explosao1
+
+} // Fim da função start
